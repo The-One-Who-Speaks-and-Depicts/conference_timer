615553ad107c4ac5438dc597099c8d3ad9d4d6ec
Ilya <szrnamerg@gmail.com>
2020-06-23 14:38:10 +0400

    coundown is sort of a done thing

--- script.js
+++ script.js
@@ -9,6 +9,16 @@
 }    
   $("#addingButton").click(function() {
       var Conf = new conf($("#name").val(), $("#date").val(), $("#stat").val(), $("#req").val());
-      $('#confs tr:last').after("<tr><td>" + Conf.name + "</td><td>" + Conf.date + "</td><td>" + Conf.statusExe + "</td><td>" + Conf.req + "</td><td>" + moment.utc(moment().diff(new Date(Conf.date))).format("YY:MM:DD:HH:mm:ss") + "</td></tr>");
+      $('#confs tr:last').after("<tr><td>" + Conf.name + "</td><td>" + Conf.date + "</td><td>" + Conf.statusExe + "</td><td>" + Conf.req + "</td><td title=\"" + Conf.date + "\" class=\"diff\">" + moment.utc(moment().diff(new Date(Conf.date))).format("YY:MM:DD:HH:mm:ss") + "</td></tr>");
   });
+
+  function countdown() {
+     var timers = document.getElementsByClassName("diff");
+        for (var i = 0; i < timers.length; i++) {
+            timers[i].innerText =  moment.utc(moment().diff(new Date($(timers[i]).attr('title')))).format("YY:MM:DD:HH:mm:ss");
+        }
+        
+    };
+
+setInterval(countdown, 1000);
   };
