e47c31db96bd825e64d84b05340865e264cbb5b2
Ilya <szrnamerg@gmail.com>
2020-06-23 18:52:01 +0400

    minutes countdown implemented

--- script.js
+++ script.js
@@ -9,13 +9,32 @@
 }    
   $("#addingButton").click(function() {
       var Conf = new conf($("#name").val(), $("#date").val(), $("#stat").val(), $("#req").val());
-      $('#confs tr:last').after("<tr><td>" + Conf.name + "</td><td>" + Conf.date + "</td><td>" + Conf.statusExe + "</td><td>" + Conf.req + "</td><td title=\"" + Conf.date + "\" class=\"diff\">" + moment.utc(moment().diff(new Date(Conf.date))).format("YY:MM:DD:HH:mm:ss") + "</td></tr>");
+      $('#confs tr:last').after("<tr><td>" + Conf.name + "</td><td>" + Conf.date + "</td><td>" + Conf.statusExe + "</td><td>" + Conf.req + "</td><td title=\"" + Conf.date + "\" class=\"diff\"></td></tr>");
   });
 
   function countdown() {
      var timers = document.getElementsByClassName("diff");
         for (var i = 0; i < timers.length; i++) {
-            timers[i].innerText =  Math.floor(moment.utc(new Date($(timers[i]).attr('title'))).diff(moment.utc(new Date()))/1000);
+            let secs = Math.floor(moment.utc(new Date($(timers[i]).attr('title'))).diff(moment.utc(new Date()))/1000);
+            if (secs > 0) {                
+                if (secs > 60) {
+                    let mins = Math.floor(secs/60);
+                    if (secs % 60 == 0) {                        
+                        timers[i].innerText = mins + " minutes, " + 0 + " seconds";
+                    }
+                    else {
+                        timers[i].innerText = Math.floor(secs/60) + " minutes, " + secs % 60 + " seconds";
+                    }
+                }
+                else {
+                    timers[i].innerText = secs + " seconds";
+                }
+                    
+            }
+            else {
+                timers[i].innerText = "Time have run out!";
+            }
+            
         }
         
     };
