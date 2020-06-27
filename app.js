628a9309a02de656d7e8a848770cedb6482fb34a
Ilya <szrnamerg@gmail.com>
2020-06-27 14:18:00 +0400

    post query on server side

--- app.js
+++ app.js
@@ -14,50 +14,53 @@ app.get('/api/confs', function(req, res) {
 })
 // получение одного пользователя по id
 app.get('/api/users/:id', function(req, res) {
-  var id = req.params.id // получаем id
-  var content = fs.readFileSync('confs.json', 'utf8')
-  var confs = JSON.parse(content)
-  var conf = null
+  var id = req.params.id; // получаем id
+  var content = fs.readFileSync('confs.json', 'utf8');
+  var confs = JSON.parse(content);
+  var conf = null;
   // находим в массиве пользователя по id
   for (var i = 0; i < confs.length; i++) {
     if (confs[i].id == id) {
-      conf = confs[i]
-      break
+      conf = confs[i];
+      break;
     }
   }
   // отправляем пользователя
   if (conf) {
-    res.send(conf)
+    res.send(conf);
   } else {
-    res.status(404).send()
+    res.status(404).send();
   }
-})
+});
 // получение отправленных данных
 app.post('/api/users', jsonParser, function(req, res) {
-  if (!req.body) return res.sendStatus(400)
+  if (!req.body) return res.sendStatus(400);
+  
+  var confName = req.body.name;
+  var confDesc = req.body.desc;
+  var confDate = req.body.date;
+  var confStat = req.body.stat;
+  var confReq = req.body.req_size;
+  var user = { name: confName, desc: confDesc, date: confDate, stat: confStat, req_size: confReq }
 
-  var userName = req.body.name
-  var userAge = req.body.age
-  var user = { name: userName, age: userAge }
-
-  var data = fs.readFileSync('users.json', 'utf8')
-  var users = JSON.parse(data)
+  var data = fs.readFileSync('confs.json', 'utf8');
+  var confs = JSON.parse(data)
 
   // находим максимальный id
   var id = Math.max.apply(
     Math,
-    users.map(function(o) {
+    confs.map(function(o) {
       return o.id
     })
   )
   // увеличиваем его на единицу
-  user.id = id + 1
+  conf.id = id + 1
   // добавляем пользователя в массив
-  users.push(user)
-  var data = JSON.stringify(users)
+  confs.push(conf)
+  var data = JSON.stringify(confs)
   // перезаписываем файл с новыми данными
-  fs.writeFileSync('users.json', data)
-  res.send(user)
+  fs.writeFileSync('confs.json', data)
+  res.send(conf)
 })
 // удаление пользователя по id
 app.delete('/api/users/:id', function(req, res) {
