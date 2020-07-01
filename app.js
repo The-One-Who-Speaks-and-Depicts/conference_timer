e2331ae70400e5481033293811fc3f0a6952804b
Ilya <szrnamerg@gmail.com>
2020-07-01 22:05:25 +0400

    legacy code cleared

--- app.js
+++ app.js
@@ -63,40 +63,7 @@ app.put('/api/confs/:id', (request, response) => {
         response.send('Conference updated successfully.');
     });
 });
-/*
-app.put('/api/confs', jsonParser, function(req, res) {
-  if (!req.body) return res.sendStatus(400)
 
-  var confId = req.body.id;
-  var confName = req.body.name;
-  var confDesc = req.body.desc;
-  var confDate = req.body.date;
-  var confStat = req.body.stat;
-  var confReq = req.body.req_size;
-
-  var data = fs.readFileSync('confs.json', 'utf8');
-  var confs = JSON.parse(data);
-  var conf;
-  for (var i = 0; i < confs.length; i++) {
-    if (confs[i].id == confId) {
-      conf = confs[i];
-      break;
-    }
-  }
-  // изменяем данные у пользователя
-  if (conf) {    
-    conf.name = confName;
-    conf.desc = confDesc;
-    conf.date = confDate;
-    conf.stat = confStat;
-    conf.req_size = confReq;
-    var data = JSON.stringify(confs);
-    fs.writeFileSync('confs.json', data);
-    res.send(conf);
-  } else {
-    res.status(404).send(conf);
-  }
-})*/
 app.listen(3000, function() {
   console.log('Сервер ожидает подключения...');
 })
--- public/js/script.js
+++ public/js/script.js
@@ -1,42 +1,4 @@
-/*
-  window.onload = function() {
-      
-    $('#stat').change(function() {        
-        if ($('#stat').val() == "пользовательский статус") {
-            document.getElementById("stat_user").style.display = "inline";
-        }
-        else if ((document.getElementById("stat_user").style.display == "inline") && ($('#stat').val() != "пользовательский статус")) {
-            document.getElementById("stat_user").style.display = "none";
-        }
-    });
-    
-    var textarea = document.querySelector('textarea');
-
-textarea.addEventListener('keyup', function(){
-  if(this.scrollTop > 0){
-    this.style.height = this.scrollHeight + "px";
-  }
-});
-      
-    
-  $("#addingButton").click(function() {
-      if ($("#name").val() == "" || $("#desc").val() == "" || $("#req_num").val() == "" || ($('#stat').val() == "пользовательский статус" && $('#stat_user').val() == "") || (moment($('#date').val()).format('YYYY/MM/DD HH:mm:ss') == "Invalid date")) {
-          alert('Заполните все поля!');
-      }
-      else {
-          var statusConf = "";
-          if ($('#stat').val() == "пользовательский статус") {
-              statusConf = $('#stat_user').val();
-          }
-          else {
-              statusConf = $('#stat').val();
-          }
-          var Conf = new conf($("#name").val(),  $("#desc").val(), $("#date").val(), statusConf, $("#req_num").val() + " " + $("#req_opt").val());
-          $('#confs tr:last').after("<tr><td class=\"identificator\"></td><td id=\"nameText\">" + Conf.name + "</td><td id=\"descText\">" + Conf.desc + "</td><td id=\"dateText\">" + moment(Conf.date).format('YYYY/MM/DD HH:mm:ss') + "</td><td id=\"status\">" + Conf.statusExe + "</td><td id=\"status\">" + Conf.req + "</td><td id=\"dateLeft\" title=\"" + Conf.date + "\" class=\"diff\"></td></tr>");
-      }
-    });*/
-
-  function countdown() {
+function countdown() {
      var timers = document.getElementsByClassName("diff");
         for (var i = 0; i < timers.length; i++) {
             timers[i].innerText = "";
